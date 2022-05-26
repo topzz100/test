bea13f3100cc2ecdf0d12da03f34aacc87526b37
Temitope Adenekan <81047123+topzz100@users.noreply.github.com>
2022-05-26 19:11:31 +0100

    Update Home.styles.js

--- src/Home/Home.styles.js
+++ src/Home/Home.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: rgb(217, 217, 243);
+  background-color: #c1f5c1;
 `;
 export const Content = styled.div`
   width: 100%;
