082a01b9ba56666909e97c1f638eea754bb1df3e
Temitope Adenekan <81047123+topzz100@users.noreply.github.com>
2022-05-26 16:20:52 +0100

    Top menu

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Category from '../Category/Category'
-import { Box, Categories, Content, Top, Wrapper } from './Main.styles'
+import { Box, Categories, Content, Top, TopMenu, Wrapper } from './Main.styles'
 
 const Main = () => {
   return (
     <>
       <Wrapper>
         <Content>
+          <TopMenu>
+            <span>
+              <i class="fa-solid fa-bars"></i>
+            </span>
+            <img src ="/images/cloudbank.png"/>
+          </TopMenu>
           <Top>
             <h4>Loans</h4>
             <p>
