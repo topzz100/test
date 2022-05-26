24eaa61df11821818c605d944e617177997a3165
Temitope Adenekan <81047123+topzz100@users.noreply.github.com>
2022-05-26 18:47:56 +0100

    Update allCategories.jsx

--- src/allCategories.jsx
+++ src/allCategories.jsx
@@ -1,49 +1,49 @@
 const allCategories = [
      {
         id: 1,
-        img: 'one.png',
+        img: '/images/one.png',
         header: 'One Month Loan',
         desc: 'Access funds for upkeep for 30 days only'
     },
      {
         id: 2,
-        img: 'two.png',
+        img: '/images/two.png',
         header: 'Working Capital Overdraft',
         desc: 'Access continuous funds to grow your business stocks and inventory'
     },
      {
         id: 3,
-        img: 'three.png',
+        img: '/images/three.png',
         header: 'Operation Overdraft',
         desc: 'Financing for your business Operation'
     },
      {
         id: 4,
-        img: 'four.png',
+        img: '/images/four.png',
         header: 'T and T spread aloan',
         desc: 'Pirchas phones, generators and vehicles on a reasonable payment plan from T and T Deals Market'
     },
      {
         id: 5,
-        img: 'five.png',
+        img: '/images/five.png',
         header: 'Infrastructure Loan',
         desc: 'Facilities for your business to purchase vehicles, assets and building upgrades'
     },
      {
         id: 6,
-        img: 'six.png',
+        img: '/images/six.png',
         header: 'Personal Loan',
         desc: 'Instan funds for your personal expenses'
     },
      {
         id: 7,
-        img: 'seven.png',
+        img: '/images/seven.png',
         header: 'Pawn Loan',
         desc: 'Get cash value and temporary sale of your items within 72 hours'
     },
      {
         id: 8,
-        img: 'eight.png',
+        img: '/images/eight.png',
         header: 'Fx line',
         desc: 'Lorem ipsum dolor sit amet, consecteur adipiscin elit'
     },
