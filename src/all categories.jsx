cc728b9d4bc1bb12938593e8ca73f1c6663662fe
Temitope Adenekan <81047123+topzz100@users.noreply.github.com>
2022-05-26 18:26:05 +0100

    Add array

---
+++ src/all categories.jsx
@@ -0,0 +1,54 @@
+const allCategories = [
+     {
+        id: 1,
+        img: 'one.png',
+        header: 'One Month Loan',
+        desc: 'Access funds for upkeep for 30 days only'
+    },
+     {
+        id: 2,
+        img: 'two.png',
+        header: 'Working Capital Overdraft',
+        desc: 'Access continuous funds to grow your business stocks and inventory'
+    },
+     {
+        id: 3,
+        img: 'three.png',
+        header: 'Operation Overdraft',
+        desc: 'Financing for your business Operation'
+    },
+     {
+        id: 4,
+        img: 'four.png',
+        header: 'T and T spread aloan',
+        desc: 'Pirchas phones, generators and vehicles on a reasonable payment plan from T and T Deals Market'
+    },
+     {
+        id: 5,
+        img: 'five.png',
+        header: 'Infrastructure Loan',
+        desc: 'Facilities for your business to purchase vehicles, assets and building upgrades'
+    },
+     {
+        id: 6,
+        img: 'six.png',
+        header: 'Personal Loan',
+        desc: 'Instan funds for your personal expenses'
+    },
+     {
+        id: 7,
+        img: 'seven.png',
+        header: 'Pawn Loan',
+        desc: 'Get cash value and temporary sale of your items within 72 hours'
+    },
+     {
+        id: 8,
+        img: 'eight.png',
+        header: 'Fx line',
+        desc: 'Lorem ipsum dolor sit amet, consecteur adipiscin elit'
+    },
+    
+   
+]
+
+export default allCategories;
